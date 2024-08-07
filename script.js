a6d835c9ac19cb4d85024ed19abb28f0dc33bc72
jjm8998 <104862620+jjm8998@users.noreply.github.com>
2024-08-07 11:46:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,32 +6,87 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "1-Entre essas frutas, qual você prefere?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "a)Acerola",
+            "b)Laranja"
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "2-Qual desses pratos é uma comida típica da Espanha,qual você prefere?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "a)Paella",
+            "b)Guacamole"
         ]
     }
 
     {
-        enunciado: "Pergunta 3",
+        enunciado: "3-Entre esses alimentos que possuem maior quantidade de vitamina B12,quais você prefere?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "a)Atum,salmão,leite",
+            "b)Banana,nozes,fígado"
         ]
     },
     {
-        enunciado: "Pergunta 4",
+        enunciado: "4-Qual destes cantores você prefere”?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "a)Michael Jackson",
+            "b)Belo"
         ]
     }
+{
+        enunciado: "5-Quais instrumentos musicais você gosta mais?",
+        alternativas: [
+            "a)Pandeiro,berimbau,cavaquinho",
+            "b)Violino,clarinete,harpa"
+        ]
+    }
+
+    {
+        enunciado: "6-Qual alternativa você acha que apresenta melhor definição sobre música?",
+        alternativas: [
+            "a)É a combinação de ritmo, harmonia e melodia, de maneira agradável ao ouvido.",
+            "b)Trata-se de um movimento controlado ou calculado que se produz pela ordenação de elementos diferentes."
+        ]
+    },
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
